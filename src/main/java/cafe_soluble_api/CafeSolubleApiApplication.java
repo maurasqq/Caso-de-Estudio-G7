704e6bd37f005f961cdd68e7c6ad47f6d695231c
maurasqq <maurasphqq@gmail.com>
2026-08-31 19:26:23 -0600

    tarea: crear proyecto base con initalizer

---
+++ src/main/java/cafe_soluble_api/CafeSolubleApiApplication.java
@@ -0,0 +1,13 @@
+package cafe_soluble_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeSolubleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeSolubleApiApplication.class, args);
+	}
+
+}
