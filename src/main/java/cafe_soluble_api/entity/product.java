e6ae91023404ce5a2fe562e5d1e2952a58429615
baxava <barkalovalexander01@gmail.com>
2026-08-31 19:46:08 -0600

    librerias agregadas, ademas de product

--- src/main/java/cafe_soluble_api/entity/product.java
+++ src/main/java/cafe_soluble_api/entity/product.java
@@ -1,7 +1,14 @@
 package cafe_soluble_api.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @Table(name = "products")
 @Getter
+@Setter
+
 public class product {
 }
